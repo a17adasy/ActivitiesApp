ca29fef52622b76ab56ba3baa21531a44d62d9b6
a17adasy <sykesftw@gmail.com>
2018-04-09 15:19:35 +0200

    started new activity

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.brom.activitiesapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
-    private int[] mountainHeights ={4478,4808,6190};
+    private String[] mountainHeights ={"4478","4808","6190"};
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
 
     @Override
@@ -15,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 1. Create a ListView as in previous assignment
+        final List<String> mNames = new ArrayList<String>(Arrays.asList(mountainNames));
+        final List<String> mLocations = new ArrayList<String>(Arrays.asList(mountainLocations));
+        final List<String> mHeights = new ArrayList<String>(Arrays.asList(mountainHeights));
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item_textview, R.id.my_item_textview, mNames);
+
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int item, long l) {
+                Intent intent = new Intent(getApplicationContext(), Mountaindetails2.class);
+                startActivity(intent);
+            }
+        });
         // 2. Create a new activity named "MountainDetailsActivity
         // 3. Create a new Layout file for the MountainDetailsActivity called
         //    "activity_mountaindetails". MountainDetailsActivity must have MainActivity as its
